2a330ccf7888ecfd514c60c0aa67bf2259dfd7eb
Eric Cheng <online.eric@gmail.com>
2024-03-18 23:28:50 +1300

    update RequestPanel

--- src/home/RequestPanel.tsx
+++ src/home/RequestPanel.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
-import { Input, Paper, Typography } from '@mui/material';
+import { Button, Input, Paper, Typography } from '@mui/material';
 import Title from '../common/Title';
 
 export default function RequestPanel() {
 	// get env var
 	const API_URL_eric_gin_server = process.env.REACT_APP_API_URL_eric_gin_server;
 	console.log('API_URL_eric_gin_server:', API_URL_eric_gin_server);
+
+	const [executeTimes, setExecuteTimes] = React.useState(500);
+
+	const callAPIs = () => {
+		console.log('callAPIs', executeTimes);
+	};
 
 	return (
 		<Paper
@@ -16,11 +22,24 @@ export default function RequestPanel() {
 				height: 240,
 			}}
 		>
-			<Title>Request submit to server</Title>
-			<Typography variant="h6">Minimal API server:</Typography>
+			<Title>Request submit to servers</Title>
+			<Typography variant="subtitle1">Minimal API server (c#):</Typography>
+			<Typography variant="subtitle1">Controller based API server (c#):</Typography>
+			<Typography variant="subtitle1">Gin API server (Golang):</Typography>
+			<div>
+				<hr />
+			</div>
 			<div style={{ display: 'flex', alignItems: 'center' }}>
 				<Typography variant="subtitle1">Execute times:</Typography>
-				<Input type="number" defaultValue={500} sx={{ width: '100px', ml: 2 }} />
+				<Input
+					type="number"
+					value={executeTimes}
+					sx={{ width: '100px', ml: 2 }}
+					onChange={(e) => setExecuteTimes(parseInt(e.target.value))}
+				/>
+				<Button variant="contained" color="primary" sx={{ ml: 2 }} onClick={callAPIs}>
+					Submit
+				</Button>
 			</div>
 		</Paper>
 	);
