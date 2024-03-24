26e965a3a58b4878327b2b5b95ee12eaf16a82c7
Eric Cheng <online.eric@gmail.com>
2024-03-24 19:11:17 +1300

    minor code improvement

--- src/home/ResponsePanel.tsx
+++ src/home/ResponsePanel.tsx
@@ -3,19 +3,19 @@ import { Paper } from '@mui/material';
 import Title from '../common/Title';
 import { DataGrid } from '@mui/x-data-grid';
 
+const columns = [
+	{ field: 'server', headerName: 'Server', width: 300 },
+	{ field: 'executionTime', headerName: 'Execution Time(ms)', width: 180 },
+	{ field: 'memoryUsed', headerName: 'Memory Used', width: 150 },
+];
+
+const rows = [
+	{ id: 1, server: 'Minimal API server (c#)', executionTime: '620', memoryUsed: 32890 },
+	{ id: 2, server: 'Controller based API server (c#)', executionTime: '615', memoryUsed: 32896 },
+	{ id: 3, server: 'Gin API server (Golang)', executionTime: '625', memoryUsed: 32800 },
+];
+
 export default function ResponsePanel() {
-	const columns = [
-		{ field: 'server', headerName: 'Server', width: 300 },
-		{ field: 'executionTime', headerName: 'Execution Time(ms)', width: 180 },
-		{ field: 'memoryUsed', headerName: 'Memory Used', width: 150 },
-	];
-
-	const rows = [
-		{ id: 1, server: 'Minimal API server (c#)', executionTime: '620', memoryUsed: 32890 },
-		{ id: 2, server: 'Controller based API server (c#)', executionTime: '615', memoryUsed: 32896 },
-		{ id: 3, server: 'Gin API server (Golang)', executionTime: '625', memoryUsed: 32800 },
-	];
-
 	return (
 		<Paper
 			sx={{
