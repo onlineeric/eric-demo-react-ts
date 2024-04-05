33d9a7aff5766b18ece3315c68f4e480e4e975fe
Eric Cheng <online.eric@gmail.com>
2024-04-05 22:46:28 +1300

    adjust grid column width

--- src/home/ResponsePanel.tsx
+++ src/home/ResponsePanel.tsx
@@ -8,8 +8,8 @@ import { format } from 'date-fns';
 
 const columns = [
 	{ field: 'server', headerName: 'Server', width: 300 },
-	{ field: 'algorithm', headerName: 'Algorithm', width: 300 },
-	{ field: 'parallelization', headerName: 'Parallelization', width: 200 },
+	{ field: 'algorithm', headerName: 'Algorithm', width: 150 },
+	{ field: 'parallelization', headerName: 'Parallelization', width: 150 },
 	{ field: 'executionTime', headerName: 'Execution Time(ms)', width: 180 },
 	{ field: 'memoryUsed', headerName: 'Memory Used', width: 150 },
 	{
