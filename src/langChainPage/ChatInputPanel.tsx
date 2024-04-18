81d847a7aea8fd4cfcba01d234b479472392c50a
Eric Cheng <online.eric@gmail.com>
2024-04-18 19:08:17 +1200

    update ChatInputPanel

--- src/langChainPage/ChatInputPanel.tsx
+++ src/langChainPage/ChatInputPanel.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
-import { Paper, Typography } from '@mui/material';
+import { Box, Fab, Grid, Paper, TextField } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+import { useAppDispatch } from '../store/hooks';
+import { addMessage } from '../store/chatHistorySlice';
 
 export default function ChatInputPanel() {
+	const [userInput, setUserInput] = React.useState('what is LangSmith in AI development?');
+	const dispatch = useAppDispatch();
+
+	const handleSend = () => {
+		dispatch(addMessage({ speaker: 'User', message: userInput, msgTime: new Date().toLocaleTimeString() }));
+	};
+
 	return (
 		<Paper
 			sx={{
@@ -11,9 +21,18 @@ export default function ChatInputPanel() {
 				height: 200,
 			}}
 		>
-			<Typography variant="body1">
-				<>what is LangSmith?</>
-			</Typography>
+			<Grid container>
+				<Grid item xs={11}>
+					<TextField fullWidth multiline rows={4} value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+				</Grid>
+				<Grid item xs={1}>
+					<Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', height: '100%' }}>
+						<Fab color="primary" aria-label="add" onClick={handleSend}>
+							<SendIcon />
+						</Fab>
+					</Box>
+				</Grid>
+			</Grid>
 		</Paper>
 	);
 }
