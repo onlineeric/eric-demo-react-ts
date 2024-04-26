4ee8bf78e579c8b485f5e94f6a7a50df4f23ad2c
Eric Cheng <online.eric@gmail.com>
2024-04-26 18:20:06 +1200

    update langchain description panel

--- src/langChainPage/DescPanel.tsx
+++ src/langChainPage/DescPanel.tsx
@@ -12,19 +12,17 @@ export default function DescPanel() {
 				p: 2,
 				display: 'flex',
 				flexDirection: 'column',
-				height: 220,
+				height: 200,
 			}}
 		>
 			<Box sx={{ overflowX: 'auto' }}>
 				<Title>LangChain Demo Page</Title>
 				<Typography variant="body1" sx={{ lineHeight: 1.7, whiteSpace: 'nowrap' }}>
-					{`Welcome! This website is my personal demo project, where I, Eric Cheng, to demonstrate my skills of Node.js, React, C#, GoLang and more.`}
+					{`This page is to demonstrate my knowledge about AI language models and their usage.`}
 					<br />
-					{`This UI is a Single Page App (SPA) built using React, TypeScript, and Material-UI.`}
+					{`Here I used LangChain to connect to my OpenAI API and created a chatbot to interact with users.`}
 					<br />
-					{`The backend connents to my other demo projects, which are built using .Net C#, and GoLang.`}
-					<br />
-					{`An LLM AI demo project is on the way! Please visit `}
+					{`Please visit `}
 					<a
 						href="https://github.com/onlineeric"
 						target="_blank"
@@ -35,27 +33,16 @@ export default function DescPanel() {
 					</a>
 				</Typography>
 				<Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1, mt: 1 }}>
-					<img src="https://img.shields.io/badge/-React-61DAFB?logo=react&logoColor=white&style=flat" alt="React" />
+					<img src="https://img.shields.io/badge/-LLM-007ACC?logo=LLM&logoColor=white&style=flat" alt="LLM" />
 					<img
-						src="https://img.shields.io/badge/-TypeScript-007ACC?logo=typescript&logoColor=white&style=flat"
-						alt="TypeScript"
+						src="https://img.shields.io/badge/-LangChain-764ABC?logo=langchain&logoColor=white&style=flat"
+						alt="LangChain"
 					/>
-					<img src="https://img.shields.io/badge/-Redux-764ABC?logo=redux&logoColor=white&style=flat" alt="Redux" />
+					<img src="https://img.shields.io/badge/-OpenAI-0081CB?logo=openai&logoColor=white&style=flat" alt="OpenAI" />
 					<img
-						src="https://img.shields.io/badge/-Material--UI-0081CB?logo=material-ui&logoColor=white&style=flat"
-						alt="Material-UI"
-					/>
-					<img src="https://img.shields.io/badge/-C%23-512BD4?logo=.net&logoColor=white" alt=".NET" />
-					<img src="https://img.shields.io/badge/-Minimal%20API-512BD4?logo=.net&logoColor=white" alt="Minimal API" />
-					<img src="https://img.shields.io/badge/-%20Web%20API-512BD4?logo=.net&logoColor=white" alt=".NET Web API" />
-					<img src="https://img.shields.io/badge/-GitHub-181717?logo=github&logoColor=white" alt="GitHub" />
-					<img
-						src="https://img.shields.io/badge/-GitHub%20Actions-2088FF?logo=github-actions&logoColor=white"
-						alt="GitHub Actions"
-					/>
-					<img src="https://img.shields.io/badge/-CI%2FCD-5C6BC0?logo=jenkins&logoColor=white" alt="CI/CD" />
-					<img src="https://img.shields.io/badge/-Azure-0089D6?logo=microsoft-azure&logoColor=white" alt="Azure" />
-					<img src="https://img.shields.io/badge/-Docker-2496ED?logo=docker&logoColor=white" alt="Docker" />
+						src="https://img.shields.io/badge/-ChatGPT-512BD4?logo=chatgpt&logoColor=white&style=flat"
+						alt="ChatGPT"
+					/>{' '}
 				</Box>
 			</Box>
 		</Paper>
