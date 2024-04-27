12e5d8480d0360f05dadf97b6f1a35d8c7ab6c4f
Eric Cheng <online.eric@gmail.com>
2024-04-27 19:18:27 +1200

    updating Home page

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
 import { Grid } from '@mui/material';
-import RequestPanel from './RequestPanel/RequestPanel';
-import ResponsePanel from './ResponsePanel';
-import ServerStatusPanel from './ServerStatusPanel';
-import ResultChartPanel from './ResultChartPanel';
 import AppDescPanel from './AppDescPanel';
 
 export default function Home() {
@@ -12,22 +8,6 @@ export default function Home() {
 			{/* App Description Panel */}
 			<Grid item xs={12}>
 				<AppDescPanel />
-			</Grid>
-			{/* Server Status Panel */}
-			<Grid item xs={12} md={5} xl={4}>
-				<ServerStatusPanel />
-			</Grid>
-			{/* Request Panel */}
-			<Grid item xs={12} md={7} xl={8}>
-				<RequestPanel />
-			</Grid>
-			{/* Response Panel */}
-			<Grid item xs={12}>
-				<ResponsePanel />
-			</Grid>
-			{/* result chat */}
-			<Grid item xs={12}>
-				<ResultChartPanel />
 			</Grid>
 		</Grid>
 	);
