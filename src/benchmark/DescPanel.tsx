95d7f217854cd980b88d55952e3c6336f85a8ba2
Eric Cheng <online.eric@gmail.com>
2024-04-28 18:36:43 +1200

    update benchmark desc

--- src/benchmark/DescPanel.tsx
+++ src/benchmark/DescPanel.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Box, Paper, Typography } from '@mui/material';
 import Title from '../common/Title';
 import { useTheme } from '@mui/material/styles';
+import BenchmarkBadges from './Badges';
 
 export default function DescPanel() {
 	const theme = useTheme();
@@ -35,29 +36,7 @@ export default function DescPanel() {
 					</a>
 				</Typography>
 			</Box>
-			<Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1, mt: 1 }}>
-				<img src="https://img.shields.io/badge/-React-61DAFB?logo=react&logoColor=white&style=flat" alt="React" />
-				<img
-					src="https://img.shields.io/badge/-TypeScript-007ACC?logo=typescript&logoColor=white&style=flat"
-					alt="TypeScript"
-				/>
-				<img src="https://img.shields.io/badge/-Redux-764ABC?logo=redux&logoColor=white&style=flat" alt="Redux" />
-				<img
-					src="https://img.shields.io/badge/-Material--UI-0081CB?logo=material-ui&logoColor=white&style=flat"
-					alt="Material-UI"
-				/>
-				<img src="https://img.shields.io/badge/-C%23-512BD4?logo=.net&logoColor=white" alt=".NET" />
-				<img src="https://img.shields.io/badge/-Minimal%20API-512BD4?logo=.net&logoColor=white" alt="Minimal API" />
-				<img src="https://img.shields.io/badge/-%20Web%20API-512BD4?logo=.net&logoColor=white" alt=".NET Web API" />
-				<img src="https://img.shields.io/badge/-GitHub-181717?logo=github&logoColor=white" alt="GitHub" />
-				<img
-					src="https://img.shields.io/badge/-GitHub%20Actions-2088FF?logo=github-actions&logoColor=white"
-					alt="GitHub Actions"
-				/>
-				<img src="https://img.shields.io/badge/-CI%2FCD-5C6BC0?logo=jenkins&logoColor=white" alt="CI/CD" />
-				<img src="https://img.shields.io/badge/-Azure-0089D6?logo=microsoft-azure&logoColor=white" alt="Azure" />
-				<img src="https://img.shields.io/badge/-Docker-2496ED?logo=docker&logoColor=white" alt="Docker" />
-			</Box>
+			<BenchmarkBadges />
 		</Paper>
 	);
 }
