c4d60b4bd44e11c137a6c37208db4b10d2e5e4a6
Eric Cheng <online.eric@gmail.com>
2024-05-02 23:59:52 +1200

    update desc statement

--- src/benchmark/DescStatement.tsx
+++ src/benchmark/DescStatement.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 
+const TEXTS = [
+	'This page is to demonstrate my full stack development knowledge of Node.js, React, C#, GoLang and more.',
+	'Sever Status panel get status from my Azure App Services, which hosts the backend API. Please wait while first time loading (free tier servers, may take a while to wake up).',
+	'Request submit to servers send request to my backend APIs, the result will be displayed in the table and the chart below.',
+	'Please send the request and check the comparison result.',
+];
+
 export default function DescStatement() {
 	return (
-		<Typography variant="body2" sx={{ lineHeight: 1.8, whiteSpace: 'nowrap' }}>
-			{`This page is to demonstrate my full stack development knowledge of Node.js, React, C#, GoLang and more.`}
-			<br />
-			{`Sever Status panel get status from my Azure App Services, which hosts the backend API. Please wait while first time loading (free tier servers, may take a while to wake up).`}
-			<br />
-			{`Request submit to servers send request to my backend APIs, the result will be displayed in the table and the chart below.`}
-			<br />
-			{`Please send the request and check the comparison result.`}
-		</Typography>
+		<div>
+			{TEXTS.map((text, index) => (
+				<Typography key={index} variant="body2" sx={{ lineHeight: 1.8, whiteSpace: 'nowrap' }}>
+					{text}
+				</Typography>
+			))}
+		</div>
 	);
 }
--- src/langChainPage/DescStatement.tsx
+++ src/langChainPage/DescStatement.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 
+const DescText = [
+	`This page is to demonstrate my knowledge about AI, LLM and LangChain, along with their applications.`,
+	`It showcases a chatbot that uses LangChain to connect to OpenAI's ChatGPT API.`,
+];
+
 export default function DescStatement() {
 	return (
-		<Typography variant="body2" sx={{ lineHeight: 1.7, whiteSpace: 'nowrap' }}>
-			{`This page is to demonstrate my knowledge about AI, LLM and LangChain, along with their applications.`}
-			<br />
-			{`It showcases a chatbot that uses LangChain to connect to OpenAI's ChatGPT API.`}
-			<br />
-		</Typography>
+		<div>
+			{DescText.map((text, index) => (
+				<Typography key={index} variant="body2" sx={{ lineHeight: 1.7, whiteSpace: 'nowrap' }}>
+					{text}
+				</Typography>
+			))}
+		</div>
 	);
 }
